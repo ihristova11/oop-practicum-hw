da5670d191782dfdb40918fbd9cc40d87a0ae5de
Irina Hristova <hristova119@gmail.com>
2020-05-23 15:35:15 +0300

    chore: update example

--- PracticumHW/PracticumHW/Startup.cpp
+++ PracticumHW/PracticumHW/Startup.cpp
@@ -20,32 +20,35 @@ int main()
 {
 	//// TESTING ONLY
 
-	//std::vector<Formula*> formulas =
-	//{
-	//	new Equation(std::vector<Element*> {new Air, new Air}, new Spirit),
-	//	new Equation(std::vector<Element*> {new Metal, new Stone}, new Gold),
-	//	new Equation(std::vector<Element*> {new Fire, new Earth}, new Metal),
-	//	new Equation(std::vector<Element*> {new Fire, new Water, new Earth}, new Gold),
-	//	new Equation(std::vector<Element*> {new Water, new Earth}, new Stone),
-	//	new Equation(std::vector<Element*> {new Spirit, new Air}, new Energy),
-	//	new Equation(std::vector<Element*> {new Gold, new Spirit, new Energy},
-	//	new PhilosophersStone),
-	//	new Decomposition(new Fire),
-	//	new Equation(std::vector<Element*> {new Energy, new Earth}, new Stone)
-	//};
+	std::vector<Formula*> formulas =
+	{
+		new Equation(std::vector<Element*> {new Air, new Air}, new Spirit),
+		new Equation(std::vector<Element*> {new Metal, new Stone}, new Gold),
+		new Equation(std::vector<Element*> {new Fire, new Earth}, new Metal),
+		new Equation(std::vector<Element*> {new Fire, new Water, new Earth}, new Gold),
+		new Equation(std::vector<Element*> {new Water, new Earth}, new Stone),
+		new Equation(std::vector<Element*> {new Spirit, new Air}, new Energy),
+		new Equation(std::vector<Element*> {new Gold, new Spirit, new Energy},
+		new PhilosophersStone),
+		new Decomposition(new Fire),
+		new Equation(std::vector<Element*> {new Energy, new Earth}, new Stone)
+	};
 
-	//std::vector<std::pair<Element*, int>> elements =
-	//{
-	//	std::pair<Element*, int> {new Earth, 3},
-	//	std::pair<Element*, int> {new Fire, 4},
-	//	std::pair<Element*, int> {new Water, 1},
-	//	std::pair<Element*, int> {new Air, 10}
-	//};
+	std::vector<std::pair<Element*, int>> elements =
+	{
+		std::pair<Element*, int> {new Earth, 3},
+		std::pair<Element*, int> {new Fire, 4},
+		std::pair<Element*, int> {new Water, 1},
+		std::pair<Element*, int> {new Air, 10}
+	};
 
-	//Book book(formulas);
-	//Alchemist alchemist(book, elements);
+	Book book(formulas);
+	Alchemist alchemist(book, elements);
 
-	//std::cout << std::boolalpha << alchemist.canComposePhilosophersStone() << std::endl;
+	std::cout << std::boolalpha << alchemist.canComposePhilosophersStone() << std::endl;
+
+	formulas.clear();
+	elements.clear();
 
 	return 0;
 }
